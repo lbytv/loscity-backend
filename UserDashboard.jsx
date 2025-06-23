28e39843922779ec7f55b5a58b82d3cbaef58123
lbytv <sofianmohmm307@gmail.com>
2025-06-23 13:47:19 +0300

    Update UserDashboard.jsx

--- UserDashboard.jsx
+++ UserDashboard.jsx
@@ -13,7 +13,7 @@ const UserDashboard = ({ user, onLogout }) => {
 
   const fetchUserApplications = async () => {
     try {
-      const response = await fetch('https://loscity-backend.onrender.com', {
+      const response = await fetch("https://loscity-backend.onrender.com/auth/ping"), {
         credentials: 'include'
       });
 
