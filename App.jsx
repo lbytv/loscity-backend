e0697212fac2b82628c0bdecae18977a46706140
lbytv <sofianmohmm307@gmail.com>
2025-06-23 13:45:02 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   const checkAuthStatus = async () => {
     try {
-      const response = await fetch('hhttps://loscity-backend.onrender.com', {
+      const response = await fetch(("https://loscity-backend.onrender.com/auth/ping"), {
         credentials: 'include'
       });
       
@@ -46,7 +46,7 @@ function App() {
 
   const handleLogout = async () => {
     try {
-      await fetch('https://loscity-backend.onrender.com', {
+      await fetch("https://loscity-backend.onrender.com/auth/ping"), {
         method: 'POST',
         credentials: 'include'
       });
